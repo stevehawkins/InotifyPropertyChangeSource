58a4960d11e6cd76bdc34e95e496e77fa6c77058
steve hawkins <steve@steve-hawkins.com>
2017-03-06 22:15:45 +0000

    Add project files.

---
+++ INotfySource/INotfySource/Models/Person.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+// in reality we would pull these from a db or service
+namespace INotfySource.Models
+{
+    public  class Person
+    {
+        public int Age { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string PersonInfo()
+        {
+            return $"{FirstName} {LastName} Age:{Age}";
+        }
+    }
+}
---
+++ INotfySource/INotfySource/viewmodels/ViewModel1.cs
@@ -0,0 +1,59 @@
+﻿using INotfySource.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace INotfySource.viewmodels
+{
+    public class ViewModel1:BaseModel
+    {
+        private Person _person;
+        public ViewModel1()
+        {
+            // set some default here for example
+            _person = new Person
+            {
+                Age = 21,
+                FirstName = "Steve",
+                LastName = "Hawkins"
+            };
+        }
+
+        public int Age
+        {
+            get { return _person.Age; }
+            set {
+                _person.Age = value;
+                OnPropertyChanged("Age");
+                OnPropertyChanged("UserInfo");
+            }
+        }
+
+        public string FirstName
+        {
+            get { return _person.FirstName; }
+            set
+            {
+                _person.FirstName = value;
+                OnPropertyChanged("FirstName");
+                OnPropertyChanged("UserInfo");
+            }
+        }
+
+        public string LastName
+        {
+            get { return _person.LastName; }
+            set
+            {
+                _person.LastName = value;
+                OnPropertyChanged("LastName");
+                OnPropertyChanged("UserInfo");
+            }
+        }
+
+        public string UserInfo
+        {
+            get { return _person.PersonInfo(); }
+        }
+   }
+}
